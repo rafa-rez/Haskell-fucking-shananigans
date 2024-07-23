4c78f3452784814dd43a05bf50dbaa34e6b90be0
Rafael Rezende <rafaelrez2004@gmail.com>
2024-07-23 19:07:37 -0300

    :sparkles: Q4

--- VAISEFUDE.hs
+++ VAISEFUDE.hs
@@ -1,1 +1,13 @@
-8=======D~~
+-- Definição da função remove
+remove :: Eq a => a -> [a] -> [a]
+remove _ [] = []
+remove x (y:ys)
+  | x == y    = ys
+  | otherwise = y : remove x ys
+
+-- Função principal para testar a função remove
+main :: IO ()
+main = do
+  let lista = [1, 2, 3, 4, 2, 5]
+  let elemento = 2
+  print $ remove elemento lista  -- Output: [1, 3, 4, 2, 5]
